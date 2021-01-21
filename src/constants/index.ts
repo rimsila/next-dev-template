784fea725ea8d7c525726c74569c726a2edde813
Sila Rim <rimsila.itc@gmail.com>
2021-01-21 10:36:59 +0700

    update base url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,7 +7,7 @@ export const IS_DEV = REACT_APP_ENV === 'dev';
 export const IS_TEST = REACT_APP_ENV === 'test';
 export const IS_PROD = !IS_DEV && !IS_TEST && IS_NODE_PROD;
 
-export const BASE_API_URL = IS_NODE_PROD && REACT_APP_BASE_API_URL;
+export const BASE_API_URL = REACT_APP_BASE_API_URL;
 
 export const COMPANY_NAME = 'Next Dev';
 export const ICON_FONT_URL = '//at.alicdn.com/t/font_2221049_8szkpgxzd38.js';
